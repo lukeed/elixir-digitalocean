94ef7455d6d1cacb204a9a661fec3c17d5fd1a4b
Luke Edwards <luke.edwards05@gmail.com>
2016-05-25 19:37:42 -0700

    install poison

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Digitalocean.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:poison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule Digitalocean.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:poison, "~> 2.2"}]
   end
 end
