dbea42ed0ac67d47ecbfe44ab67d63c5c9d19c59
Luke Edwards <luke.edwards05@gmail.com>
2016-05-28 21:27:55 -0700

    add empty action module

---
+++ lib/digitalocean/action.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.Action do
+	@moduledoc """
+	API methods for interacting with the `Action` module.
+
+	See the [Action Reference](https://developers.digitalocean.com/documentation/v2/#account).
+	"""
+end
