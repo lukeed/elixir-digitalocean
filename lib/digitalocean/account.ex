6e38b55a4afbad7ea38aeda1ff3c2f1d228fa627
Luke Edwards <luke.edwards05@gmail.com>
2016-05-28 21:27:47 -0700

    add empty account module

---
+++ lib/digitalocean/account.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.Account do
+	@moduledoc """
+	API methods for interacting with the `Account` module.
+
+	See the [Account Reference](https://developers.digitalocean.com/documentation/v2/#account).
+	"""
+end
