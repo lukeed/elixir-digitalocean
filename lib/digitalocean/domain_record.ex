f93a82c1bd28a9107dc8fe2adb116c9bff957e94
Luke Edwards <luke.edwards05@gmail.com>
2016-05-28 21:28:28 -0700

    add empty domain-record module

---
+++ lib/digitalocean/domain_record.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.Domain.Record do
+	@moduledoc """
+	API methods for interacting with the `Domain Record` module.
+
+	See the [Domain Record Reference](https://developers.digitalocean.com/documentation/v2/#domain-records).
+	"""
+end
