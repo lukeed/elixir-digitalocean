7be87e764f889a05bd56484d40599bb277fc95f4
Luke Edwards <luke.edwards05@gmail.com>
2016-05-28 21:28:05 -0700

    add empty domain module

---
+++ lib/digitalocean/domain.ex
@@ -0,0 +1,12 @@
+defmodule DigitalOcean.Domain do
+	@moduledoc """
+	API methods for interacting with the `Domain` module.
+
+	See the [Domain Reference](https://developers.digitalocean.com/documentation/v2/#domains).
+	"""
+
+	def index(), do:
+	def show(), do:
+	def new(), do:
+	def delete(), do:
+end
