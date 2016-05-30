6d4712ce54ece5bef49114c50bcdf1ef799e54e5
Luke Edwards <luke.edwards05@gmail.com>
2016-05-29 21:28:58 -0700

    add empty droplet-action module

---
+++ lib/digitalocean/droplet_action.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.Droplet.Action do
+	@moduledoc """
+	API methods for interacting with the `Droplet Action` module.
+
+	See the [Droplet Action Reference](https://developers.digitalocean.com/documentation/v2/#droplet-actions).
+	"""
+end
