ed9d74670a0ddcc49b0fb17f1e30dfdddcea88d4
Luke Edwards <luke.edwards05@gmail.com>
2016-05-29 21:29:34 -0700

    add empty size module

---
+++ lib/digitalocean/size.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.Size do
+	@moduledoc """
+	API methods for interacting with the `Size` module.
+
+	See the [Size Reference](https://developers.digitalocean.com/documentation/v2/#sizes).
+	"""
+end
