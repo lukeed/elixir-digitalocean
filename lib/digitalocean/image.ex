2810661d13ce52cd54e740ee1cd5b771eee4dc8c
Luke Edwards <luke.edwards05@gmail.com>
2016-05-29 21:29:08 -0700

    add empty image module

---
+++ lib/digitalocean/image.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.Image do
+	@moduledoc """
+	API methods for interacting with the `Image` module.
+
+	See the [Image Reference](https://developers.digitalocean.com/documentation/v2/#images).
+	"""
+end
