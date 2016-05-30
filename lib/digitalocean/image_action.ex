e58c8508cea7e1da7e33b440a34996ad8ae6336f
Luke Edwards <luke.edwards05@gmail.com>
2016-05-29 21:29:17 -0700

    add empty image-action module

---
+++ lib/digitalocean/image_action.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.Image.Action do
+	@moduledoc """
+	API methods for interacting with the `Image Action` module.
+
+	See the [Image Action Reference](https://developers.digitalocean.com/documentation/v2/#image-actions).
+	"""
+end
