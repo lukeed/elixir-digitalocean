e11dda9748c6f6a5d6a81edf9a86f5c092cf2310
Luke Edwards <luke.edwards05@gmail.com>
2016-05-29 21:29:25 -0700

    add empty region module

---
+++ lib/digitalocean/region.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.Region do
+	@moduledoc """
+	API methods for interacting with the `Region` module.
+
+	See the [Region Reference](https://developers.digitalocean.com/documentation/v2/#regions).
+	"""
+end
