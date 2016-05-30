8b6a2aff7e374a73a50752f0d2dec0284c1d3f7a
Luke Edwards <luke.edwards05@gmail.com>
2016-05-29 21:28:48 -0700

    add empty droplet module

---
+++ lib/digitalocean/droplet.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.Droplet do
+	@moduledoc """
+	API methods for interacting with the `Droplet` module.
+
+	See the [Droplet Reference](https://developers.digitalocean.com/documentation/v2/#droplets).
+	"""
+end
