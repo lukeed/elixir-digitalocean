f9e638a841c5d809e64d97b02f6607e30257cd82
Luke Edwards <luke.edwards05@gmail.com>
2016-05-30 21:30:10 -0700

    add empty (ssh) key module

---
+++ lib/digitalocean/key.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.Key do
+	@moduledoc """
+	API methods for interacting with the `SSH Key` module.
+
+	See the [SSH Key Reference](https://developers.digitalocean.com/documentation/v2/#ssh-keys).
+	"""
+end
