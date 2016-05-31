1a648d1d06bce7a984e29dc19c683147ccae4aa4
Luke Edwards <luke.edwards05@gmail.com>
2016-05-30 21:29:56 -0700

    add empty tag module

---
+++ lib/digitalocean/tag.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.Tag do
+	@moduledoc """
+	API methods for interacting with the `Tag` module.
+
+	See the [Tag Reference](https://developers.digitalocean.com/documentation/v2/#tags).
+	"""
+end
