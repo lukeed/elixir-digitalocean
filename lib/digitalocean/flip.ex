a45a0b0839eff9bcd38ab937b4d2b96b4d7a69cd
Luke Edwards <luke.edwards05@gmail.com>
2016-05-31 21:30:54 -0700

    add empty fl(oating) ip module

---
+++ lib/digitalocean/flip.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.FlIP do
+	@moduledoc """
+	API methods for interacting with the `Floating IP` module.
+
+	See the [Floating IP Reference](https://developers.digitalocean.com/documentation/v2/#floating-ips).
+	"""
+end
