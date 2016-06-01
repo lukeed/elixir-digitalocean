892d34ba11ecc88bcb99f1ea0ed624a038e9e3a0
Luke Edwards <luke.edwards05@gmail.com>
2016-05-31 21:31:08 -0700

    add empty fl(oating)-ip-action module

---
+++ lib/digitalocean/flip_action.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.FlIP.Action do
+	@moduledoc """
+	API methods for interacting with the `Floating IP Action` module.
+
+	See the [Floating IP Action Reference](https://developers.digitalocean.com/documentation/v2/#floating-ip-actions).
+	"""
+end
