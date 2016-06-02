35bdc5107db30a892a7bd10da0e10930f5588166
Luke Edwards <luke.edwards05@gmail.com>
2016-06-01 21:31:32 -0700

    add empty (block)storage module

---
+++ lib/digitalocean/storage.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.Storage do
+	@moduledoc """
+	API methods for interacting with the `Block Storage` module.
+
+	See the [Block Storage Reference](https://developers.digitalocean.com/documentation/v2/#block-storage).
+	"""
+end
