9cd778a6302ba10cd900e72229672378b48a5537
Luke Edwards <luke.edwards05@gmail.com>
2016-06-01 21:31:40 -0700

    add empty (block)storage-action module

---
+++ lib/digitalocean/storage_action.ex
@@ -0,0 +1,7 @@
+defmodule DigitalOcean.Storage.Action do
+	@moduledoc """
+	API methods for interacting with the `Block Storage Action` module.
+
+	See the [Block Storage Action Reference](https://developers.digitalocean.com/documentation/v2/#block-storage-actions).
+	"""
+end
