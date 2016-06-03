572cc654ae8a8df0636ba3eda26384ea2acec03a
Luke Edwards <luke.edwards05@gmail.com>
2016-06-02 21:47:35 -0700

    add mix package info

--- mix.exs
+++ mix.exs
@@ -1,34 +1,33 @@
 defmodule DigitalOcean.Mixfile do
   use Mix.Project
 
-  def project do
-    [app: :digitalocean,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
-  end
+  def project do [
+    app: :digitalocean,
+    version: "0.1.0",
+    elixir: "~> 1.3",
+    description: "Elixir wrapper for the Digital Ocean API v2.",
+    build_embedded: Mix.env == :prod,
+    start_permanent: Mix.env == :prod,
+    deps: deps()
+  ] end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:poison, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    [{:poison, "~> 2.2"},
-     {:httpoison, "~> 0.9.0"},
-     {:ex_doc, "~> 0.13.0", only: :dev}]
-  end
+  defp deps do [
+    {:poison, "~> 2.2"},
+    {:httpoison, "~> 0.9.0"},
+    {:ex_doc, "~> 0.13.0", only: :dev}
+  ] end
+
+  defp package do [
+    name: :digitalocean,
+    maintainers: ["Luke Edwards"],
+    licenses: ["MIT"],
+    links: %{
+      "GitHub" => "https://github.com/lukeed/elixir-digitalocean",
+      "Docs" => "https://hex.pm/packages/digitalocean"
+    }
+  ] end
 end
