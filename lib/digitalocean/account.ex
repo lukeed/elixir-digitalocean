2628d9cfd8090390bea3ee7c9ffd494e8bdd4dde
Luke Edwards <luke.edwards05@gmail.com>
2016-06-03 22:46:14 -0700

    stub the account module methods

--- lib/digitalocean/account.ex
+++ lib/digitalocean/account.ex
@@ -4,4 +4,17 @@ defmodule DigitalOcean.Account do
 
 	See the [Account Reference](https://developers.digitalocean.com/documentation/v2/#account).
 	"""
+
+	@doc """
+	Get information about the Account associated with the token.
+
+	## Example
+		iex> DigitalOcean.Account.show
+	"""
+	def show, do: get("account")
+
+	@doc """
+	Similar to `show/0` but returns the response body only.
+	"""
+	def show!, do: show |> body
 end
