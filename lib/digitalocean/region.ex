b00bc09570748f7f4c18548ea3dfd0ea40726f1a
Luke Edwards <luke.edwards05@gmail.com>
2016-06-05 00:01:53 -0700

    stub the region module methods

--- lib/digitalocean/region.ex
+++ lib/digitalocean/region.ex
@@ -4,4 +4,14 @@ defmodule DigitalOcean.Region do
 
 	See the [Region Reference](https://developers.digitalocean.com/documentation/v2/#regions).
 	"""
+
+	@doc """
+	List all Regions.
+	"""
+	def list, do: get("regions")
+
+	@doc """
+	Similar to `list/0` but returns the response body only.
+	"""
+	def list!, do: list |> body
 end
