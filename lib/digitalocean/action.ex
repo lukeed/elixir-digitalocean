ffc683531f74a443b5a8949182de7f9eebb98483
Luke Edwards <luke.edwards05@gmail.com>
2016-06-04 23:18:33 -0700

    stub the actions module methods

--- lib/digitalocean/action.ex
+++ lib/digitalocean/action.ex
@@ -4,4 +4,24 @@ defmodule DigitalOcean.Action do
 
 	See the [Action Reference](https://developers.digitalocean.com/documentation/v2/#account).
 	"""
+
+	@doc """
+	List all Actions.
+	"""
+	def list, do: get("actions")
+
+	@doc """
+	Similar to `list/0` but returns the response body only.
+	"""
+	def list!, do: list |> body
+
+	@doc """
+	Get an Action.
+	"""
+	def show(action_id), do: get("actions/#{action_id}")
+
+	@doc """
+	Similar to `show/1` but returns the response body only.
+	"""
+	def show!(action_id), do: show(action_id) |> body
 end
