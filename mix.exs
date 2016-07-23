a929784ed921a3250056aec14ca47d22bd858e18
Luke Edwards <luke.edwards05@gmail.com>
2016-07-23 13:51:48 -0700

    0.0.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DigitalOcean.Mixfile do
 
   def project do [
     app: :digitalocean,
-    version: "0.1.0",
+    version: "0.0.1",
     elixir: "~> 1.3",
     description: "(WIP) Elixir wrapper for the Digital Ocean API v2.",
     build_embedded: Mix.env == :prod,
