e174ac29c69760bbfa67e7b1f76f731bc8cd1bb2
Luke Edwards <luke.edwards05@gmail.com>
2016-07-24 00:24:25 -0700

    add flip.action module methods

--- lib/digitalocean/flip_action.ex
+++ lib/digitalocean/flip_action.ex
@@ -4,4 +4,20 @@ defmodule DigitalOcean.FlIP.Action do
 
 	See the [Floating IP Action Reference](https://developers.digitalocean.com/documentation/v2/#floating-ip-actions).
 	"""
+
+	import DigitalOcean, only: [get: 1, post: 2, body: 1, full: 1]
+
+	def list(ip), do: url(ip) |> get |> full
+	def list!(ip), do: list(ip) |> body
+
+	def show(ip, act_id), do: get("#{url(ip)}/#{act_id}") |> full
+	def show!(ip, act_id), do: show(ip, act_id) |> body
+
+	def assign(ip, droplet_id), do: url(ip) |> post(%{type: "assign", droplet_id: droplet_id}) |> full
+	def assign!(ip, droplet_id), do: assign(ip, droplet_id) |> body
+
+	def unassign(ip), do: url(ip) |> post(%{type: "unassign"}) |> full
+	def unassign!(ip), do: unassign(ip) |> body
+
+	defp url(ip), do: "floating_ips/#{ip}/actions"
 end
