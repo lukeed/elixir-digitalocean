c0b891533458c9322dd4e9ceb8f967c5d20e296e
Luke Edwards <luke.edwards05@gmail.com>
2016-07-24 00:03:38 -0700

    add image.action module methods

--- lib/digitalocean/image_action.ex
+++ lib/digitalocean/image_action.ex
@@ -4,4 +4,20 @@ defmodule DigitalOcean.Image.Action do
 
 	See the [Image Action Reference](https://developers.digitalocean.com/documentation/v2/#image-actions).
 	"""
+
+	import DigitalOcean, only: [get: 1, post: 2, body: 1, full: 1]
+
+	def list(img_id), do: url(img_id) |> get |> full
+	def list!(img_id), do: list(img_id) |> body
+
+	def transfer(img_id, region), do: url(img_id) |> post(%{type: "transfer", region: region}) |> full
+	def transfer!(img_id, region), do: transfer(img_id, region) |> body
+
+	def convert(img_id), do: url(img_id) |> post(%{type: "convert"}) |> full
+	def convert!(img_id), do: convert(img_id) |> body
+
+	def show(img_id, act_id), do: get("#{url(img_id)}/#{act_id}") |> full
+	def show!(img_id, act_id), do: show(img_id, act_id) |> body
+
+	defp url(id), do: "images/#{id}/actions"
 end
