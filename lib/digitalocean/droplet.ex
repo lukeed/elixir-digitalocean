2bd948761a79fdfc24427a71a1639eb6ddc52d58
Luke Edwards <luke.edwards05@gmail.com>
2016-07-27 01:07:03 -0700

    add more droplet methods

--- lib/digitalocean/droplet.ex
+++ lib/digitalocean/droplet.ex
@@ -9,19 +9,44 @@ defmodule DigitalOcean.Droplet do
 	List all Droplets.
 	"""
 	def list, do: get("droplets") |> full
-
-	@doc """
-	Similar to `list/0` but returns the response body only.
-	"""
 	def list!, do: list |> body
 
 	@doc """
 	Get all Droplets with a given Tag. Same as `DigitalOcean.Tag.droplets/1`.
 	"""
 	def list(tag), do: get("droplets?tag_name=#{tag}") |> full
+	def list!(tag), do: list(tag) |> body
 
 	@doc """
-	Similar to `list/1` but returns the response body only.
+	Create a new Droplet.
 	"""
-	def list!(tag), do: list(tag) |> body
+	# def create(name, ip), do: post("droplets", %{name: name, ip_address: ip}) |> full
+	# def create!(name, ip), do: create(name, ip) |> body
+
+	@doc """
+	Get an existing Droplet.
+	"""
+	def show(droplet_id), do: url(droplet_id) |> get |> full
+	def show!(droplet_id), do: show(droplet_id) |> body
+
+	@doc """
+	List available Kernels for a Droplet.
+	"""
+	def show_kernels(droplet_id), do: url(droplet_id, "kernels") |> get |> full
+	def show_kernels!(droplet_id), do: show_kernels(droplet_id) |> body
+
+	@doc """
+	List snapshots for a Droplet.
+	"""
+	def show_snapshots(droplet_id), do: url(droplet_id, "/snapshots") |> get |> full
+	def show_snapshots!(droplet_id), do: show_snapshots(droplet_id) |> body
+
+	@doc """
+	List available backups for a Droplet.
+	"""
+	def show_backups(droplet_id), do: url(droplet_id, "backups") |> get |> full
+	def show_backups!(droplet_id), do: show_backups(droplet_id) |> body
+
+	defp url(id), do: "droplets/#{id}"
+	defp url(id, path), do: "#{url(id)}/#{path}"
 end
