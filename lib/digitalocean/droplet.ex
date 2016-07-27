8344ed9ae9f826f72ffab1fabdb5fff12de6b09a
Luke Edwards <luke.edwards05@gmail.com>
2016-07-27 00:04:08 -0700

    add list methods to droplet

--- lib/digitalocean/droplet.ex
+++ lib/digitalocean/droplet.ex
@@ -4,4 +4,24 @@ defmodule DigitalOcean.Droplet do
 
 	See the [Droplet Reference](https://developers.digitalocean.com/documentation/v2/#droplets).
 	"""
+
+	@doc """
+	List all Droplets.
+	"""
+	def list, do: get("droplets") |> full
+
+	@doc """
+	Similar to `list/0` but returns the response body only.
+	"""
+	def list!, do: list |> body
+
+	@doc """
+	Get all Droplets with a given Tag. Same as `DigitalOcean.Tag.droplets/1`.
+	"""
+	def list(tag), do: get("droplets?tag_name=#{tag}") |> full
+
+	@doc """
+	Similar to `list/1` but returns the response body only.
+	"""
+	def list!(tag), do: list(tag) |> body
 end
