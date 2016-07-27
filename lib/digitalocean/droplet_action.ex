fda5953bf9979a8c315d186d2e5c6fdc280e7c5f
Luke Edwards <luke.edwards05@gmail.com>
2016-07-26 23:58:16 -0700

    stub droplet.action.list

--- lib/digitalocean/droplet_action.ex
+++ lib/digitalocean/droplet_action.ex
@@ -4,4 +4,7 @@ defmodule DigitalOcean.Droplet.Action do
 
 	See the [Droplet Action Reference](https://developers.digitalocean.com/documentation/v2/#droplet-actions).
 	"""
+
+	def list(droplet_id), do: get("droplets/#{droplet_id}/actions") |> full
+	def list!(droplet_id), do: list(droplet_id) |> body
 end
