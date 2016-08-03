a229b3aebe808e1a1366799ea956e4d27d7bcbcf
Luke Edwards <luke.edwards05@gmail.com>
2016-08-03 00:03:51 -0700

    stub out droplet.action methods

--- lib/digitalocean/droplet_action.ex
+++ lib/digitalocean/droplet_action.ex
@@ -5,8 +5,33 @@ defmodule DigitalOcean.Droplet.Action do
 	See the [Droplet Action Reference](https://developers.digitalocean.com/documentation/v2/#droplet-actions).
 	"""
 
-	import DigitalOcean, only: [get: 1, body: 1, full: 1]
+	import DigitalOcean, only: [get: 1, post: 2, body: 1, full: 1]
 
-	def list(droplet_id), do: get("droplets/#{droplet_id}/actions") |> full
-	def list!(droplet_id), do: list(droplet_id) |> body
+	def list(id), do: url(id) |> get |> full
+	def list!(id), do: list(id) |> body
+
+	def show(id, act_id), do: "#{url(id)}/#{act_id}" |> get |> full
+	def show!(id, act_id), do: show(id, act_id) |> body
+
+	def enable_backups(id), do: url(id) |> post(%{type: "enable_backups"}) |> full
+	def disable_backups(id), do: url(id) |> post(%{type: "disable_backups"}) |> full
+
+	def shutdown(id), do: url(id) |> post(%{type: "disable_backups"}) |> full
+	def reboot(id), do: url(id) |> post(%{type: "disable_backups"}) |> full
+	def cycle(id), do: url(id) |> post(%{type: "disable_backups"}) |> full
+	def off(id), do: url(id) |> post(%{type: "disable_backups"}) |> full
+	def on(id), do: url(id) |> post(%{type: "disable_backups"}) |> full
+
+	# reset_password
+	def restore(id, image), do:
+	# resize
+	def rebuild(id, image), do:
+	def snapshot(id, name), do:
+	def rename(id, name), do:
+
+	# change_kernel
+	# enable_ipv6
+	# enable_pnetwork
+
+	defp url(id), do: "droplets/#{id}/actions"
 end
