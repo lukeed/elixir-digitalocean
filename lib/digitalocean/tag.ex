c62749900c5ba95d236acaa22799a75582cb5926
Luke Edwards <luke.edwards05@gmail.com>
2016-08-03 15:19:52 -0700

    finish tag module

--- lib/digitalocean/tag.ex
+++ lib/digitalocean/tag.ex
@@ -5,7 +5,7 @@ defmodule DigitalOcean.Tag do
 	See the [Tag Reference](https://developers.digitalocean.com/documentation/v2/#tags).
 	"""
 
-	import DigitalOcean, only: [get: 1, post: 2, put: 2, del: 1, body: 1, full: 1]
+	import DigitalOcean, only: [get: 1, post: 2, put: 2, del: 1, del: 2, body: 1, full: 1]
 
 	def list, do: get("tags") |> full
 	def list!, do: list |> body
@@ -13,13 +13,13 @@ defmodule DigitalOcean.Tag do
 	def create(name), do: post("tags", %{name: name}) |> full
 	def create!(name), do: create(name) |> body
 
-	def show(name), do: get("tags/#{name}") |> full
+	def show(name), do: url(name) |> get |> full
 	def show!(name), do: show(name) |> body
 
-	def update(old, new), do: put("tags/#{old}", %{name: new}) |> full
+	def update(old, new), do: url(old) |> put(%{name: new}) |> full
 	def update!(old, new), do: update(old, new) |> body
 
-	def delete(name), do: del("tags/#{name}") |> full
+	def delete(name), do: url(name) |> del |> full
 	def delete!(name), do: delete(name) |> body
 
 	@doc """
@@ -28,6 +28,16 @@ defmodule DigitalOcean.Tag do
 	def droplets(name), do: get("droplets?tag_name=#{name}") |> full
 	def droplets!(name), do: droplets(name) |> body
 
-	# TODO: `Resource` struct?
-	# TODO: tag & untag resource
+	@doc """
+	Add the Tag to each Droplet in the list, via their IDs.
+	"""
+	def tag(name, list), do: post("#{url(name)}/resources", merger(list))
+
+	@doc """
+	Remove the Tag from a list of Droplets, via their IDs.
+	"""
+	def untag(name, list), do: del("#{url(name)}/resources", merger(list))
+
+	defp url(tag), do: "tags/#{tag}"
+	defp merger(list), do: Enum.map(list, &Map.put(&1, :resource_type, "droplet"))
 end
