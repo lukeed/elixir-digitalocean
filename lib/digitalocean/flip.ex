3b2906ec998f8673bc029c74bf71bb82001e4953
Luke Edwards <luke.edwards05@gmail.com>
2016-08-04 10:43:14 -0700

    add flip methods

--- lib/digitalocean/flip.ex
+++ lib/digitalocean/flip.ex
@@ -4,4 +4,18 @@ defmodule DigitalOcean.FlIP do
 
 	See the [Floating IP Reference](https://developers.digitalocean.com/documentation/v2/#floating-ips).
 	"""
+
+	import DigitalOcean, only: [get: 1, post: 2, del: 1, body: 1, full: 1]
+
+	def list, do: get("floating_ips") |> full
+	def list!, do: list |> body
+
+	def show(ip), do: get("floating_ips/#{ip}") |> full
+	def show!(ip), do: show(ip) |> body
+
+	def create(data), do: post("floating_ips", data) |> full
+	def create!(data), do: create(data) |> body
+
+	def delete(ip), do: del("floating_ips/#{ip}") |> full
+	def delete!(ip), do: delete(ip) |> body
 end
