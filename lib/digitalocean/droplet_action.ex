248857acb087a67e51786f7db505929e4ae07445
Luke Edwards <luke.edwards05@gmail.com>
2016-08-04 00:04:46 -0700

    condense simple droplet actions to enum loop

--- lib/digitalocean/droplet_action.ex
+++ lib/digitalocean/droplet_action.ex
@@ -13,25 +13,22 @@ defmodule DigitalOcean.Droplet.Action do
 	def show(id, act_id), do: "#{url(id)}/#{act_id}" |> get |> full
 	def show!(id, act_id), do: show(id, act_id) |> body
 
-	def enable_backups(id), do: url(id) |> post(%{type: "enable_backups"}) |> full
-	def disable_backups(id), do: url(id) |> post(%{type: "disable_backups"}) |> full
-
-	def shutdown(id), do: url(id) |> post(%{type: "disable_backups"}) |> full
-	def reboot(id), do: url(id) |> post(%{type: "disable_backups"}) |> full
-	def cycle(id), do: url(id) |> post(%{type: "disable_backups"}) |> full
-	def off(id), do: url(id) |> post(%{type: "disable_backups"}) |> full
-	def on(id), do: url(id) |> post(%{type: "disable_backups"}) |> full
-
 	# reset_password
-	def restore(id, image), do:
-	# resize
-	def rebuild(id, image), do:
-	def snapshot(id, name), do:
-	def rename(id, name), do:
-
+	# def restore(id, image), do:
+	# # resize
+	# def rebuild(id, image), do:
+	# def snapshot(id, name), do:
+	# def rename(id, name), do:
 	# change_kernel
-	# enable_ipv6
-	# enable_pnetwork
+
+	# simple POST methods
+	["enable_backups", "disable_backups", "reboot", "shutdown",
+	 "power_cycle", "power_off", "power_on", "enable_ipv6",
+	 "enable_private_networking"]
+	 	|> Enum.each(fn evt ->
+			def unquote(:"#{evt}")(id), do: url(id) |> post(%{type: unquote(evt)}) |> full
+			def unquote(:"#{evt}!")(id), do: unquote(:"#{evt}")(id) |> body
+		end)
 
 	defp url(id), do: "droplets/#{id}/actions"
 end
